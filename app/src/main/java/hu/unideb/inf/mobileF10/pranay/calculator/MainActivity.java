78284c92aa0a587ef72265539e110072b548a732
Pranay Harjai <pranayharjai7@gmail.com>
2021-12-22 22:07:02 +0100

    Bug correction: 0 was appended at first, now it is not.

--- app/src/main/java/hu/unideb/inf/mobileF10/pranay/calculator/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/mobileF10/pranay/calculator/MainActivity.java
@@ -21,11 +21,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleClick(View view) {    //view is superclass of all android components. (E.G. buttons, Textview...)
-        Button btn = (Button)view;          //So you can type caste View into any component of android.
-        switch(btn.getText().toString()){
-            case "CE": result.setText("0"); break;
-            case "=": result.setText(calculateResult()); break;
-            default: result.append(btn.getText());
+        Button btn = (Button) view;          //So you can type caste View into any component of android.
+        switch (btn.getText().toString()) {
+            case "CE":
+                result.setText("0");
+                break;
+            case "=":
+                result.setText(calculateResult());
+                break;
+            default: {
+                if (result.getText().toString().equalsIgnoreCase("0"))
+                    result.setText(btn.getText());
+                else
+                    result.append(btn.getText());
+            }
+
         }
     }
 
@@ -34,17 +44,21 @@ public class MainActivity extends AppCompatActivity {
         char operator;
         String resultString = result.getText().toString();
         op1 = Integer.parseInt(resultString.split("[+\\-/*]")[0]); //if you want to use many delimiters, put them in [].
-                                                                        //you can set [1-9] to use all numbers between 1 to 9
-                                                                        //so to mask "-" minus, use "//" double backslash
+        //you can set [1-9] to use all numbers between 1 to 9
+        //so to mask "-" minus, use "//" double backslash
         op2 = Integer.parseInt(resultString.split("[+\\-/*]")[1]);
         int operatorPosition = resultString.split("[+\\-/*]")[0].length();
         operator = resultString.charAt(operatorPosition);
 
-        switch (operator){
-            case '+': return ""+(op1+op2);
-            case '-': return ""+(op1-op2);
-            case '*': return ""+(op1*op2);
-            case '/': return ""+(op1/op2);
+        switch (operator) {
+            case '+':
+                return "" + (op1 + op2);
+            case '-':
+                return "" + (op1 - op2);
+            case '*':
+                return "" + (op1 * op2);
+            case '/':
+                return "" + (op1 / op2);
         }
         return "";
     }
